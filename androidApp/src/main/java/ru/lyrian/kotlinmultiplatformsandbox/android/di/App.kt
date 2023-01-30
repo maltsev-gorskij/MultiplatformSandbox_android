a9a91ec814524c9285b891b9734a42ba8916dd2c
Dmitry Maltsev-Gorskij <maltsev-gorskij@yandex.ru>
2023-01-30 12:35:14 +0300

    - Refactored Koin initializer invocation

--- androidApp/src/main/java/ru/lyrian/kotlinmultiplatformsandbox/android/di/App.kt
+++ androidApp/src/main/java/ru/lyrian/kotlinmultiplatformsandbox/android/di/App.kt
@@ -5,16 +5,16 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.logger.Level
 import ru.lyrian.kotlinmultiplatformsandbox.android.BuildConfig
-import ru.lyrian.kotlinmultiplatformsandbox.core.di.initializeKoin
+import ru.lyrian.kotlinmultiplatformsandbox.core.di.KoinInitializer
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        initializeKoin {
+        KoinInitializer {
             if (BuildConfig.DEBUG) androidLogger(Level.DEBUG)
             androidContext(this@App)
             modules(androidModules())
-        }
+        }.initializeKoin()
     }
 }
