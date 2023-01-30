2ef5d75cca22b4a337f5e6a4829c7976b66bb344
Dmitry Maltsev-Gorskij <maltsev-gorskij@yandex.ru>
2023-01-30 15:58:44 +0300

    - Deleted unused inject

--- androidApp/src/main/java/ru/lyrian/kotlinmultiplatformsandbox/android/di/App.kt
+++ androidApp/src/main/java/ru/lyrian/kotlinmultiplatformsandbox/android/di/App.kt
@@ -1,20 +1,22 @@
 package ru.lyrian.kotlinmultiplatformsandbox.android.di
 
 import android.app.Application
+import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
-import org.koin.core.logger.Level
-import ru.lyrian.kotlinmultiplatformsandbox.android.BuildConfig
 import ru.lyrian.kotlinmultiplatformsandbox.core.di.KoinInitializer
+import ru.lyrian.kotlinmultiplatformsandbox.core.initializers.AppInitializer
 
 class App : Application() {
+    private val appInitializer: AppInitializer by inject()
+
     override fun onCreate() {
         super.onCreate()
 
         KoinInitializer {
-            if (BuildConfig.DEBUG) androidLogger(Level.DEBUG)
             androidContext(this@App)
             modules(androidModules())
         }.initializeKoin()
+
+        appInitializer.init()
     }
 }
